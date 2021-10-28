383b84ea3b470a6c523373fc90762e21e743cf81
Emmanuel Okon <emmaudo27@gmail.com>
2021-10-28 20:09:55 +0100

    footer added

--- JS/booking.js
+++ JS/booking.js
@@ -9,7 +9,7 @@ document.getElementById('booking').innerHTML += `
                 <h3>book appointment</h3>
                 <input type="text" placeholder="your name" class="box">
                 <input type="number" placeholder="your number" class="box">
-                <input type="email" placeholder="your email" class="box">
+                <input type="email" placeholder="Your Email" class="box email">
                 <input type="date" class="box">
                 <input type="submit" value="book now" class="btn">
             </form>
